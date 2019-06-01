75ea302d4ae170e61735b46d5a0619e627c53442
fed-gren <deveb22@gmail.com>
2019-06-02 07:51:53 +0900

    refactor: remove dispatch
    - 잘못 사용한 dispatch 제거

--- src/containers/ModalContainer.js
+++ src/containers/ModalContainer.js
@@ -1,20 +1,19 @@
 import React from "react";
-import { connect, useSelector, useDispatch } from "react-redux";
+import { connect, useSelector } from "react-redux";
 import Modal from "../components/Modal";
 import { show, close } from "../store/modules/modal";
 
 const ModalContainer = props => {
-  const dispatch = useDispatch();
   const { message } = props;
   const isShow = useSelector(state => state.modal.isShow);
   const invalidOrder = useSelector(state => state.player.invalidOrder);
   const gameOver = useSelector(state => state.player.gameOver);
 
-  if(invalidOrder) dispatch({type: "modal/SHOW", message: "아직 상대가 고르기 전입니다."});
+  if(invalidOrder) props.show("아직 상대가 고르기 전입니다.");
 
-  if(gameOver === 1) dispatch({type: "modal/SHOW", message: "Player 1 승리!"});
-  else if(gameOver === 2) dispatch({type: "modal/SHOW", message: "Player 2 승리!"});
-  else if(gameOver === 3) dispatch({type: "modal/SHOW", message: "비겼습니다."});
+  if(gameOver === 1) props.show("Player 1 승리!");
+  else if(gameOver === 2) props.show("Player 2 승리!");
+  else if(gameOver === 3) props.show( "비겼습니다.");
   return (isShow && <Modal message={message} />);
 };
 
