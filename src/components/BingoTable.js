bc4148b8da497295c436f8743ae73a46fb4c0347
fed-gren <deveb22@gmail.com>
2019-06-01 18:56:51 +0900

    fix: block class name
    - 빙고 블럭 클래스명 checked로 정하도록 수정

--- src/components/BingoTable.js
+++ src/components/BingoTable.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "../styles/BingoTable.css";
 
-export default ({nums}) => {
+export default ({nums, checked}) => {
+  const blockClassNames = nums.map((num, index) => {
+    if(checked[index]) return "bingo_block checekd";
+    else return "bingo_block";
+  });
+  console.log(blockClassNames);
   return (
     <div className="bingo_container">
       {nums.map((data, index) => (
-        <div className="bingo_block" key={index}>
+        <div className={blockClassNames[index]} key={index}>
           {data}
         </div>
       ))}
