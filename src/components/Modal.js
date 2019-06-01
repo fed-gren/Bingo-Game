6c9c4940d222d5bd474f5d9f6443ecc8cba4a86a
fed-gren <deveb22@gmail.com>
2019-06-01 22:37:32 +0900

    fix: message, button
    - 메시지, 버튼 생성 및 스타일 수정

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -5,7 +5,8 @@ export default ({ type, message }) => {
   return (
     <div className="modal_background">
       <div className="modal">
-
+        <p className="modal_message">{message}</p>
+        <button className="btn_ok">확인</button>
       </div>
     </div>
   );
