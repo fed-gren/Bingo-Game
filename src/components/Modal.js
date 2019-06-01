b09af35feb252b0ab11a8e0b77d65f843609a5a3
fed-gren <deveb22@gmail.com>
2019-06-01 22:19:59 +0900

    feat: create a modal component
    - 안내 메시지를 위한 modal 컴포넌트 마크업, 스타일 작성 완료

---
+++ src/components/Modal.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/Modal.css";
+
+export default ({ type, message }) => {
+  return (
+    <div className="modal_background">
+      <div className="modal">
+
+      </div>
+    </div>
+  );
+};
--- src/components/Player.js
+++ src/components/Player.js
@@ -3,17 +3,16 @@ import PlayerLayout from "./PlayerLayout";
 import BingoTable from "./BingoTable";
 import ScoreBoard from "./ScoreBoard";
 import Notice from "./Notice";
+import Modal from "./Modal";
 
-export default ({nums, message, playerNumber, checked}) => {
+export default ({ nums, message, playerNumber, checked }) => {
   return (
     <PlayerLayout>
       <Notice message={message} />
       <div className="player_name">player {playerNumber}</div>
-      <BingoTable
-        nums={nums}
-        checked={checked}
-      />
+      <BingoTable nums={nums} checked={checked} />
       <ScoreBoard />
+      <Modal />
     </PlayerLayout>
   );
 };
