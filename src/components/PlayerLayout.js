e5794e0ab032b31423e33ac113509db2692c8f53
fed-gren <deveb22@gmail.com>
2019-06-01 12:09:00 +0900

    feat: add notice zone
    - player 메시지를 위한 notice 작성

--- src/components/PlayerLayout.js
+++ src/components/PlayerLayout.js
@@ -2,5 +2,10 @@ import React from "react";
 import "../styles/PlayerLayout.css";
 
 export default ({ children }) => {
-  return <div className="player_layout">{children}</div>;
+  return (
+    <div className="player_layout">
+      <div className="notice">blah blah</div>
+      {children}
+    </div>
+  );
 };
