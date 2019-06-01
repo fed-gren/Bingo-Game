005567c6503f2ea3dc7980ea620d668e2415493f
fed-gren <deveb22@gmail.com>
2019-06-01 21:40:16 +0900

    fix: modify action name
    - 선택된 숫자를 체크하는 액션으로 수정했으므로 GET_ 에서 CHECK_ 로 수정

--- src/components/BingoTable.js
+++ src/components/BingoTable.js
@@ -12,7 +12,7 @@ export default ({ nums, checked }) => {
 
   const handleClick = e => {
     let selectedNum = Number(e.target.innerText);
-    dispatch({ type: "player/GET_SELECTED_NUM", selectedNum: selectedNum});
+    dispatch({ type: "player/CHECK_SELECTED_NUM", selectedNum: selectedNum});
   };
 
   return (
--- src/store/modules/player.js
+++ src/store/modules/player.js
@@ -1,4 +1,6 @@
 const NUM_BINGO_BLOCK = 25;
+const playerArr = [1, 2];
+
 const genRandomNum = MAX => {
   return Math.floor(Math.random() * MAX + 1);
 };
@@ -15,16 +17,23 @@ const getIndex = (arr, selectedNum) => arr.indexOf(selectedNum);
 
 //선택된 인덱스 true로 수정
 const checkSelectedIndex = (arr, index) => {
-  if(arr[index] === false) arr[index] = true;
-}
+  if (arr[index] === false) {
+    arr[index] = true;
+  }
+};
 
 // 액션 타입 정의
 const NUMS_GENERATOR = "player/NUMS_GENERATOR";
-const GET_SELECTED_NUM = "player/GET_SELECTED_NUM";
+const CHECK_SELECTED_NUM = "player/CHECK_SELECTED_NUM";
+const TOGGLE = "player/TOGGLE";
 
 // 액션 생성 함수 정의
 export const numsGenerator = () => ({ type: NUMS_GENERATOR });
-export const getSelectedNum = selectedNum => ({ type: GET_SELECTED_NUM, selectedNum });
+export const getSelectedNum = selectedNum => ({
+  type: CHECK_SELECTED_NUM,
+  selectedNum
+});
+export const toggle = () => ({ type: TOGGLE });
 
 // 초기 상태 정의
 const initialState = {
@@ -32,7 +41,10 @@ const initialState = {
   player1Checked: [],
   player2Nums: [...initCells],
   player2Checked: [],
-  selectedNum: null
+  selectedNum: null,
+  now: playerArr[0],
+  player1Message: `빙고 한 판 할까요?`,
+  player2Message: `빙고 한 판 할까요?`,
 };
 
 // 리듀서 정의
@@ -58,16 +70,27 @@ export default (state = initialState, action) => {
         player2Nums: [...player2NewNums],
         player2Checked: [...initCheckedArr]
       };
-    case GET_SELECTED_NUM:
-      checkSelectedIndex(state.player1Checked, getIndex(state.player1Nums, action.selectedNum));
-      checkSelectedIndex(state.player2Checked, getIndex(state.player2Nums, action.selectedNum));
+    case CHECK_SELECTED_NUM:
+      checkSelectedIndex(
+        state.player1Checked,
+        getIndex(state.player1Nums, action.selectedNum)
+      );
+      checkSelectedIndex(
+        state.player2Checked,
+        getIndex(state.player2Nums, action.selectedNum)
+      );
       return {
         ...state,
         selectedNum: action.selectedNum,
         player1Checked: [...state.player1Checked],
-        player2Checked: [...state.player2Checked]
-      }
-      return state;
+        player2Checked: [...state.player2Checked],
+        now: state.now
+      };
+    case TOGGLE:
+      return {
+        ...state,
+        now: state.now === playerArr[0] ? playerArr[1] : playerArr[0],
+      };
     default:
       return state;
   }
